e47211af9f2cb75fa8d8e6e42c56daf9efa16df6
sudosurootdev <sudosurootdev@gmail.com>
2015-06-29 04:37:13 -0400

    Add Compiler Optimization flags
    Change-Id: Ife642092d0b96cd8322acc6ebc9939b8a8fa7e60
    Signed-off-by: sudosurootdev <sudosurootdev@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,6 +34,15 @@ TARGET_CPU_VARIANT := krait
 TARGET_KERNEL_TOOLCHAIN_VERSION := linaro-4.9
 TARGET_KERNEL_USE_AOSP_TOOLCHAIN := false
 
+# Compiler Optimization
+TARGET_GLOBAL_CFLAGS += -O3 -mfpu=neon-vfpv4 -mfloat-abi=softfp -mvectorize-with-neon-quad
+
+TARGET_GLOBAL_CPPFLAGS += -O3 -mfpu=neon-vfpv4 -mfloat-abi=softfp -mvectorize-with-neon-quad
+
+CLANG_TARGET_GLOBAL_CFLAGS += -Ofast -mfpu=neon-vfpv4 -mfloat-abi=softfp
+
+CLANG_TARGET_GLOBAL_CPPFLAGS += -Ofast -mfpu=neon-vfpv4 -mfloat-abi=softfp
+
 TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
 
 # Bootloader
